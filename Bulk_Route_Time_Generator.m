c05927979a100c5b2817372054e3079832a2f5e0
Thermospore <35350819+Thermospore@users.noreply.github.com>
2021-10-13 09:12:29 -0800

    add secret caveman times

--- Bulk_Route_Time_Generator.m
+++ Bulk_Route_Time_Generator.m
@@ -5,7 +5,7 @@ clear all
 % set village (no caps, to be safe)
 %  - selects proper data files
 %  - triggers any village specific computations
-village = 'sailor';
+village = 'caveman';
 secret = true;
 
 % set time to clip through soveena door when it isn't unlocked
